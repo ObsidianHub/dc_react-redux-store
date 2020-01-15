51c3ca88b1b4a3be8278297ac91b6a4997c275dc
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-01-15 16:38:50 +0200

    create pages basic templates

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import { HomePage, CartPage } from "../pages";
+
 import "./app.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Switch>
+      <Route path="/" component={HomePage} exact />
+      <Route path="/cart" component={CartPage} />
+    </Switch>
+  );
 };
 
 export default App;
