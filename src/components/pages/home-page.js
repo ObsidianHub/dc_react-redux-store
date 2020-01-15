a2fcc2c9751439c1b55ad860bd4e2f89561ee16a
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-01-15 16:47:38 +0200

    write book-list logic and book-list-item, edit homePage for test

--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -1,7 +1,23 @@
 import React from "react";
+import BookList from "../book-list";
 
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return (
+    <BookList
+      books={[
+        {
+          id: 1,
+          title: "Production-Ready Microservices",
+          author: "Susan J. Fowler"
+        },
+        {
+          id: 2,
+          title: "Release It!",
+          author: "Michael T. Nygard"
+        }
+      ]}
+    />
+  );
 };
 
 export default HomePage;
