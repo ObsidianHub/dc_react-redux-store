8443d0c164d1a3ac2e9613b0bdfe9efcdea8732d
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-09-30 18:06:41 +0300

    destructure our code in reducers, divided our updates for book list and shopping cart as two diff functions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,13 @@
 const initialState = {
-  books: [],
-  loading: true,
-  error: null,
-  cartItems: [],
-  orderTotal: 220,
+  bookList: {
+    books: [],
+    loading: true,
+    error: null,
+  },
+  shoppingCart: {
+    cartItems: [],
+    orderTotal: 0,
+  },
 };
 
 const updateCartItems = (cartItems, item, idx) => {
@@ -30,7 +34,10 @@ const updateCartItem = (book, item = {}, quantity) => {
 };
 
 const updateOrder = (state, bookId, quantity) => {
-  const { books, cartItems } = state;
+  const {
+    bookList: { books },
+    shoppingCart: { cartItems },
+  } = state;
 
   const book = books.find(({ id }) => id === bookId);
   const itemIndex = cartItems.findIndex(({ id }) => id === bookId);
@@ -38,9 +45,50 @@ const updateOrder = (state, bookId, quantity) => {
 
   const newItem = updateCartItem(book, item, quantity);
   return {
-    ...state,
+    orderTotal: 0,
     cartItems: updateCartItems(cartItems, newItem, itemIndex),
   };
+};
+
+const updateBookList = (state, action) => {
+  switch (action.type) {
+    case "FETCH_BOOKS_REQUEST":
+      return {
+        books: [],
+        loading: true,
+        error: null,
+      };
+    case "FETCH_BOOKS_SUCCESS":
+      return {
+        books: action.payload,
+        loading: false,
+        error: null,
+      };
+    case "FETCH_BOOKS_FAILURE":
+      return {
+        books: [],
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state.bookList;
+  }
+};
+
+const updateShoppingCart = (state, action) => {
+  switch (action.type) {
+    case "BOOK_ADDED_TO_CART":
+      return updateOrder(state, action.payload, 1);
+    case "BOOK_REMOVED_FROM_CART":
+      return updateOrder(state, action.payload, -1);
+    case "ALL_BOOK_REMOVED_FROM_CART":
+      const item = state.shoppingCart.cartItems.find(
+        ({ id }) => id === action.payload
+      );
+      return updateOrder(state, action.payload, -item.count);
+    default:
+      return state.shoppingCart;
+  }
 };
 
 const reducer = (state = initialState, action) => {
@@ -48,33 +96,21 @@ const reducer = (state = initialState, action) => {
 
   switch (action.type) {
     case "FETCH_BOOKS_REQUEST":
-      return {
-        ...state,
-        books: [],
-        loading: true,
-        error: null,
-      };
     case "FETCH_BOOKS_SUCCESS":
-      return {
-        ...state,
-        books: action.payload,
-        loading: false,
-        error: null,
-      };
     case "FETCH_BOOKS_FAILURE":
       return {
         ...state,
-        books: [],
-        loading: false,
-        error: action.payload,
+        bookList: updateBookList(state, action),
       };
+
     case "BOOK_ADDED_TO_CART":
-      return updateOrder(state, action.payload, 1);
     case "BOOK_REMOVED_FROM_CART":
-      return updateOrder(state, action.payload, -1);
     case "ALL_BOOK_REMOVED_FROM_CART":
-      const item = state.cartItems.find(({ id }) => id === action.payload);
-      return updateOrder(state, action.payload, -item.count);
+      return {
+        ...state,
+        shoppingCart: updateShoppingCart(state, action),
+      };
+
     default:
       return state;
   }
