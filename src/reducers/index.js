37c969ca4eee5efdb6e2e9f78248e4b73fc067e8
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-09-30 11:23:44 +0300

    modify our code to compact it

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -14,6 +14,17 @@ const updateCartItems = (cartItems, item, idx) => {
   return [...cartItems.slice(0, idx), item, ...cartItems.slice(idx + 1)];
 };
 
+const updateCartItem = (book, item) => {
+  const { id = book.id, count = 0, title = book.title, total = 0 } = item;
+
+  return {
+    id,
+    title,
+    count: count + 1,
+    total: total + book.price,
+  };
+};
+
 const reducer = (state = initialState, action) => {
   console.log(action.type);
 
@@ -45,26 +56,10 @@ const reducer = (state = initialState, action) => {
       const itemIndex = state.cartItems.findIndex(({ id }) => id === bookId);
       const item = state.cartItems[itemIndex];
 
-      let newItem;
-
-      if (item) {
-        newItem = {
-          ...item,
-          count: item.count + 1,
-          total: item.total + book.price,
-        };
-      } else {
-        newItem = {
-          id: book.id,
-          title: book.title,
-          count: 1,
-          total: book.price,
-        };
-      }
-
+      const newItem = updateCartItem(book, item);
       return {
         ...state,
-        cartItems: updateCartItems(state.cartItems, item, itemIndex),
+        cartItems: updateCartItems(state.cartItems, newItem, itemIndex),
       };
 
     default:
